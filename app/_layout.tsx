683780d2a885a21759fb186399afdc322ca3dbea
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2025-02-17 19:42:36 +0300

    `Moved QueryClientProvider and GestureHandlerRootView to correct positions in RootLayout component`

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,8 @@
-import { useEffect } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useEffect } from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 const queryClient = new QueryClient();
 
@@ -18,10 +19,12 @@ export default function RootLayout() {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      </Stack>
-      <StatusBar style="auto" />
+      <GestureHandlerRootView>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        </Stack>
+        <StatusBar style="auto" />
+      </GestureHandlerRootView>
     </QueryClientProvider>
   );
 }
